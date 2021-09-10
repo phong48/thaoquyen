d0a2ee95294409915148bbd084105a4fda09846c
phong48 <90468636+phong48@users.noreply.github.com>
2021-09-10 23:15:01 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1631288765,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/spritefont.png",
+		"images/sprite-sheet0.png",
+		"images/check-sheet0.png",
+		"images/check-sheet1.png",
+		"images/check-sheet2.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
